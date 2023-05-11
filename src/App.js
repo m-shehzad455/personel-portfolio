d81f82d1eb6e68d3bc8fe48504209994fe1b3269
m-shehzad455 <shehzuali5@gmail.com>
2023-05-11 15:54:16 +0500

    basic app setup and update navabar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+      <div className="max-w-screen-xl mx-auto">
+      <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {logo}  from "../../assets/index";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+          <img src={logo} alt="logo" />
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
