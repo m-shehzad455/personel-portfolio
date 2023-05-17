2ff4edf3537e76002240a26e1cd0c52be4eb3b75
m-shehzad455 <shehzuali5@gmail.com>
2023-05-18 02:32:10 +0500

    left banner files

---
+++ src/components/features/Features.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>
+      <section id='features' className=' w-full  h-[800px] pb-20 flex  border-b-[1px] border-b-black'>
+       Features
+    </section>
+    </div>
+  )
+}
+
+export default Features
