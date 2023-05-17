fafe93b7ae0d5853a2d6f0adcaffa96afb8e6362
m-shehzad455 <shehzuali5@gmail.com>
2023-05-17 18:00:05 +0500

    update navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {logo}  from "../../assets/index";
+import {navLinksdata} from "../../constants"
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='w-full h-20 mx-auto flex justify-between items-center font-titlFont'>
       <div>
           <img src={logo} alt="logo" />
       </div>
-      <div></div>
+      <div>
+        <ul className='flex items-center gap-10'>
+          {navLinksdata.map(({_id,title,link}) => (
+            <li className='text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300' key={_id}>
+              {title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
