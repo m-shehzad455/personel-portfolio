3ca66c9cbaa3f210e2bd256a69d727d0ef4df23b
m-shehzad455 <shehzuali5@gmail.com>
2023-05-19 20:40:54 +0500

    update features section

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,13 +1,54 @@
-import React from 'react'
+import React from "react";
+import Title from "../layouts/Title";
+import Card from "./Card";
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe , FaBars} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
 
 const Features = () => {
   return (
-    <div>
-      <section id='features' className=' w-full  h-[800px] pb-20 flex  border-b-[1px] border-b-black'>
-       Features
-    </section>
-    </div>
-  )
-}
+    <section
+      id="features"
+      className=" w-full py-20 border-b-[1px] border-b-black"
+    >
+      <Title title="Features" desc="What i do" />
+      <div className="grid grid-cols-3 gap-20">
+        <Card
+           icon={<FaBars />}
+          title="Business Stratagy"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+          
+        />
 
-export default Features
+        <Card
+          icon={<AiFillAppstore />}
+          title="App Development"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        />
+         <Card
+          icon={<SiProgress />}
+          title="SEO Optimisation"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        />
+         <Card
+          icon={<FaMobile  />}
+          title="Mobile Development"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        />
+         <Card
+          icon={<SiAntdesign  />}
+          title="UX Design"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        />
+         <Card
+          icon={<FaGlobe />}
+          title="Hosting Websites"
+          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+        />
+        
+      </div>
+    </section>
+  );
+};
+
+export default Features;
