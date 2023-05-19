370a0fb90cac37e7f634801e312df441206b978d
m-shehzad455 <shehzuali5@gmail.com>
2023-05-20 01:03:57 +0500

    update resume section

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Banner from "./components/banner/Banner";
 import Features from "./components/features/Features";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
+import Resume from "./components/resume/Resume";
 
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
-      <div className="max-w-screen-xl mx-auto">
       <Navbar />
+      <div className="max-w-screen-xl mx-auto px-16">
       <Banner />
       <Features />
       <Projects />
+      <Resume />
       </div>
     </div>
   );
