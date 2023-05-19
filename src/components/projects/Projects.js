69d06e3cb50f408c15be2c047dff984a52c76716
m-shehzad455 <shehzuali5@gmail.com>
2023-05-19 21:51:39 +0500

    update projects section

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Title from "../layouts/Title";
+import ProjectCard from "./ProjectCard";
+import { projectOne, projectTwo, projectThree } from "../../assets/index";
+
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title
+          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          desc="My Projects"
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-14">
+        <ProjectCard
+          title="SOCIAL MEDIA CLONE"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectOne}
+        />
+        <ProjectCard
+          title="E-commerce Website"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectTwo}
+        />
+        <ProjectCard
+          title="Chatting App"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectThree}
+        />
+        <ProjectCard
+          title="SOCIAL MEDIA CLONE"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectThree}
+        />
+        <ProjectCard
+          title="E-commerce Website"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectOne}
+        />
+        <ProjectCard
+          title="Chatting App"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={projectTwo}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
