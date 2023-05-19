386f2fd392cc732c01ea196c166a98ea87629f6a
m-shehzad455 <shehzuali5@gmail.com>
2023-05-19 06:08:44 +0500

    update banner section

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,83 +1,25 @@
 import React from "react";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import LeftBanner from "./LeftBanner";
+import { bannerImg } from "../../assets/index";
 
 const Banner = () => {
-  const [text] = useTypewriter({
-    words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
-    loop: true,
-    typeSpeed: 20,
-    deleteSpeed: 10,
-    delaySpeed: 2000,
-  });
-
   return (
     <section
       id="home"
-      className=" w-full  py-20 items-center border-b-[1px] font-titleFont border-b-black"
+      className=" w-full pt-10 pb-20 flex  items-center border-b-[1px] font-titleFont border-b-black"
     >
-      <div className="w-1/2  flex flex-col gap-20">
-        <div className="flex flex-col gap-5">
-          <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
-          <h1 className="text-6xl font-bold text-white">
-            Hi, I'm{" "}
-            <span className="text-designColor capitalize">John doe</span>
-          </h1>
-          <h2 className="text-4xl font-bold text-white">
-            a <span>{text}</span>
-            <Cursor
-              cursorBlinking="false"
-              cursorStyle="|"
-              cursorColor="#ff014f"
-            />
-          </h2>
-          <p className="text-base font-bodyFont leading-6 tracking-wide">
-            I use animation as a third dimension by which to simplify
-            experiences and kuiding thro each and every interaction. I'm not
-            adding motion just to spruce things up, but doing it in ways that.
-          </p>
-        </div>
-        <div className="flex justify-between"> 
-          <div>
-            <h2 className="text-base uppercase font-titleFont mb-4">
-              Find me in
-            </h2>
-
-            <div className="flex gap-4">
-              <span className="bannerIcon">
-                <FaFacebookF />
-              </span>
-              <span className="bannerIcon">
-                <FaTwitter />
-              </span>
-              <span className="bannerIcon">
-                <FaLinkedinIn />
-              </span>
-            </div>
-          </div>
-          <div>
-          <h2 className="text-base uppercase font-titleFont mb-4">
-            BEST SKILL ON
-          </h2>
-          <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaReact />
-            </span>
-            <span className="bannerIcon">
-              <SiNextdotjs />
-            </span>
-            <span className="bannerIcon">
-              <SiTailwindcss />
-            </span>
-            <span className="bannerIcon">
-              <SiFigma />
-            </span>
-          </div>
-          </div>
-          </div>
+      <LeftBanner />
+      <div className="w-1/2 flex justify-center items-center relative">
+        <img
+          src={bannerImg}
+          alt="bannerImg"
+          className="w-[500px] h-[680px] z-10"
+        />
+        <div
+          className="absolute bottom-0 w-[500px] h-[500px]
+         bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"
+        ></div>
       </div>
-      <div className="w-1/2"></div>
     </section>
   );
 };
